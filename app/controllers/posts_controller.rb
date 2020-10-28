9143d66aae24264b8a73148dc18f419e997a1c06
Moto-kawakatsu <kawakatu1227@gmail.com>
2020-10-28 22:19:13 +0900

    showアクションの追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,9 +25,14 @@ class PostsController < ApplicationController
         post.update(post_params)
     end
 
-      private
-      def post_params
+    def show
+        @post = Post.find(params[:id])
+    end
+
+    private
+
+    def post_params
         params.require(:post).permit(:name, :image, :text)
-      end
+    end
 
 end
