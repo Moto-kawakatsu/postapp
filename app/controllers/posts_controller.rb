39033fa0356086136b3deef97ec3dac63d82d6f6
Moto-kawakatsu <kawakatu1227@gmail.com>
2020-10-28 22:54:51 +0900

    postsテーブルにuser_idカラムの追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -34,7 +34,7 @@ class PostsController < ApplicationController
     private
 
     def post_params
-        params.require(:post).permit(:name, :image, :text)
+        params.require(:post).permit(:name, :image, :text).merge(user_id: current_user.id)
     end
 
     def set_tweet
