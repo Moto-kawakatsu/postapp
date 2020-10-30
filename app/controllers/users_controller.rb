d059433e20b194fbbd56ad215f009b5f6eca4c5f
Moto-kawakatsu <kawakatu1227@gmail.com>
2020-10-30 11:15:19 +0900

    修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
     def show
         user = User.find(params[:id])
         @nickname = user.nickname
+        @email = user.email
         @posts = user.posts
       end
 end
