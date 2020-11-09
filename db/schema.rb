1ed1b0e5ddac0eb5e0f699df81a20f4b72351a30
Moto-kawakatsu <kawakatu1227@gmail.com>
2020-11-09 17:21:40 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -50,14 +50,14 @@ ActiveRecord::Schema.define(version: 2020_10_29_130530) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "text", null: false
-    t.text "image", null: false
     t.string "title", null: false
+    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "nickname", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -65,7 +65,6 @@ ActiveRecord::Schema.define(version: 2020_10_29_130530) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
