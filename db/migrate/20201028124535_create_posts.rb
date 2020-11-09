12092b2958a0d525bdb235edaa9d807489b05da9
Moto-kawakatsu <kawakatu1227@gmail.com>
2020-11-09 17:21:24 +0900

    postsテーブルにuser_idカラムを追加、imageカラムを削除

--- db/migrate/20201028124535_create_posts.rb
+++ db/migrate/20201028124535_create_posts.rb
@@ -2,8 +2,8 @@ class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
       t.string :text,  null: false
-      t.text :image,   null: false
       t.string :title, null: false
+      t.integer :user_id, foreign_key: true
       t.timestamps
     end
   end
