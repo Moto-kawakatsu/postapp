823b9798c9ceab163f8a08bf675ddbffad023c5e
Moto-kawakatsu <kawakatu1227@gmail.com>
2020-11-12 23:46:19 +0900

    postsテーブルにnull: falseの追加

--- db/migrate/20201028124535_create_posts.rb
+++ db/migrate/20201028124535_create_posts.rb
@@ -3,7 +3,7 @@ class CreatePosts < ActiveRecord::Migration[6.0]
     create_table :posts do |t|
       t.string :text,  null: false
       t.string :title, null: false
-      t.integer :user_id, foreign_key: true
+      t.integer :user_id, foreign_key: true, null: false
       t.timestamps
     end
   end
