f9ef53565a06f9940016476cc679b19abf6cb550
Moto-kawakatsu <kawakatu1227@gmail.com>
2020-11-19 21:28:40 +0900

    新規ユーザー登録のバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
@@ -8,8 +6,17 @@ class User < ApplicationRecord
   has_many :comments
   has_many :likes, dependent: :destroy
   has_many :liked_posts, through: :likes, source: :post
+
   def already_liked?(post)
     self.likes.exists?(post_id: post.id)
- end
+  end
 
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[\w-]{6,128}+\z/i
+
+  with_options presence: true do
+    validates :nickname
+    validates :email
+    validates :password, length: {minimum: 6 }, format: { with: VALID_PASSWORD_REGEX}
+  end
+  
 end
